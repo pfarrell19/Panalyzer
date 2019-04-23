a8b63c63428c50e1689468d546a5255bfbd83d11
Pat-Farrell-17 <38262281+Pat-Farrell-17@users.noreply.github.com>
2019-04-22 21:28:33 -0700

    Added match winner stats

--- data.py
+++ data.py
@@ -28,9 +28,10 @@ def get_matches():
     return matches['id']
 
 
-# Given a match ID, pull the data for that match and return it as a pandas DataFrame
-def get_match_stats(matchID):
-    header = {"accept" : "application/vnd.api+json"}
+# Extract the telemetry json url from the match as well as the winning player's IDs (written to winner_outfile)
+def get_match_data(matchID, winner_outfile):
+
+    header = {"accept": "application/vnd.api+json"}
     apireq = requests.get("https://api.pubg.com/shards/steam/matches/%s" % matchID, headers=header)
 
     # API response is structured: { data : {...}
@@ -39,15 +40,31 @@ def get_match_stats(matchID):
     # included contains links to the actual objects about the match (look up by ID from data)
 
     # get the data
-    match_df = pd.DataFrame(apireq.json()['data'])
+    match_data = pd.DataFrame(apireq.json()['data'])
+    match_included = pd.DataFrame(apireq.json()['included'])
 
     # Get the id of the telemetry object to look up in the included array
-    assets = pd.DataFrame(match_df.loc['assets', 'relationships'])['data'].apply(pd.Series)
+    assets = pd.DataFrame(match_data.loc['assets', 'relationships'])['data'].apply(pd.Series)
     telemetry_id = assets[assets.type == "asset"]['id'].values[0]
 
+    # Get the winning roster
+    rosters = pd.DataFrame(match_data.loc['rosters', 'relationships'])['data'].apply(pd.Series)
+    rosters_included = match_included[match_included.type == 'roster']
+    combined = rosters_included.merge(rosters, on='id')
+    combined = pd.concat([combined.drop(['attributes', 'relationships'], axis=1),
+                                combined['attributes'].apply(pd.Series),
+                                combined['relationships'].apply(pd.Series)], axis=1)
+
+    winning_team = combined[combined.won == 'true']
+    winning_participants = json_normalize(winning_team['participants'].values[0], record_path='data')
+
+    # Now get the data (stats, etc) related to the winning players from the included array
+    winning_participants_data = match_included[match_included.id.isin(winning_participants.id)]
+    # And write it to disk
+    winning_participants_data.to_csv(winner_outfile, header=False, mode="a")
+
     # Get the telemetry object from the included array
-    match_data = pd.DataFrame(apireq.json()['included'])
-    telemetry_object = match_data[match_data.id == telemetry_id]
+    telemetry_object = match_included[match_included.id == telemetry_id]
 
     # Return the url of the telemetry object
     return telemetry_object['attributes'].apply(pd.Series)['URL']
