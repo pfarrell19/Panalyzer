1104275b2d206fe9a83b13a15e56f1a02cdaa30c
andrewia <offers@andrewiam.com>
2019-06-05 11:19:36 -0700

    Fix imports and formatting, copy notebook changes (scaling x-values)

--- recommender.py
+++ recommender.py
@@ -1,25 +1,29 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split, cross_val_score, cross_validate, KFold
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler, scale
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import Pipeline
 
 
 # note that I am assuming the target is in the last position of the dataframe
 # additionally, I am assuming that the list has already been filtered(ie. we are only training on the top players)
 # additionally, my current assumption is the data has already been transformed into non-categorical data
 def train_model(df, max_k):
-    X = df.iloc[:, :-1].copy()#assuming our target is at the very end of the dataframe
+    x = df.iloc[:, :-1].copy()  # assuming our target is at the very end of the dataframe
     y = df.iloc[:, -1].copy()
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size= .2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2)
+    x_train = scale(x_train)
+    x_test = scale(x_test)
     k_scores = []
-    for k in range(1,max_k):
+    for k in range(1, max_k):
         knn = KNeighborsClassifier(n_neighbors=k)
         cv_scores = cross_val_score(knn, x_train, y_train, cv=10, scoring='accuracy')
         k_scores.append(cv_scores.mean())
 
     max_score = max(k_scores)
 
-    #     get the arg max of the score array to determine the optimnal value for k
+    #  get the arg max of the score array to determine the optimal value for k
     k_opt = None
     for i in range(len(k_scores)):
         if k_scores[i] == max_score:
@@ -33,8 +37,7 @@ def train_model(df, max_k):
     kf.get_n_splits(x_train)
     training_scores = []
 
-
-    knn.fit(x_train, y_train)#we get about -2.something places per map accuracy
+    knn.fit(x_train, y_train)  # we get about -2.something places per map accuracy
     # testing_accuracy = (knn.predict(x_test) - y_test).mean()
 
     predictions = knn.predict(x_test)
@@ -45,28 +48,28 @@ def train_model(df, max_k):
 
 
 def player_path_model(df, max_k):
-    #TODO: get x and y from df
-    
+    # TODO: get x and y from df
+
     # Zone Data
     x = None
 
     # Player Position Category
     y = None
-    
+
     best_score = 0
     best_model = None
-    
-    #Hyperparameter Tuning
+
+    # Hyperparameter Tuning
     for k in range(max_k):
         vec = DictVectorizer(sparse=False)
         scaler = StandardScaler()
         model = KNeighborsClassifier(n_neighbors=k)
-        pipeline = Pipeline([('vec', vec), 
-                            ('scaler', scaler),
-                            ('fit', model)])
-        
-        score = cross_val_score(pipeline, 
-                                x, 
+        pipeline = Pipeline([('vec', vec),
+                             ('scaler', scaler),
+                             ('fit', model)])
+
+        score = cross_val_score(pipeline,
+                                x,
                                 y,
                                 cv=5, scoring='accuracy').mean()
 
@@ -76,22 +79,21 @@ def player_path_model(df, max_k):
 
     print("Best Accuracy Score: " + str(best_score))
     return best_model
-        
+
+
 # preprocess the dataframe
 def preprocess_data(drop_data):
     drop_data = drop_data.dropna()
-    drop_data = drop_data.drop(columns = ['player'])#probably don't need to include the player in the model
-    drop_data = drop_data.drop(columns = ['drop_loc_raw'])#probably don't need to include the player in the model
+    drop_data = drop_data.drop(columns=['player'])  # probably don't need to include the player in the model
+    drop_data = drop_data.drop(columns=['drop_loc_raw'])  # probably don't need to include the player in the model
     drop_data = drop_data.dropna()
     drop_data = drop_data[drop_data['rank'] <= 5]
-    labelencoder_X=LabelEncoder()
-    X = drop_data.iloc[:,:].values
-    drop_data['flight_path'] = labelencoder_X.fit_transform(X[:, 1])
-    drop_data['map'] = labelencoder_X.fit_transform(X[:, 2])
-    drop_data = drop_data.drop(columns = ['rank'])
+    labelencoder_x = LabelEncoder()
+    x = drop_data.iloc[:, :].values
+    drop_data['flight_path'] = labelencoder_x.fit_transform(x[:, 1])
+    drop_data['map'] = labelencoder_x.fit_transform(x[:, 2])
+    drop_data = drop_data.drop(columns=['rank'])
     drop_data = drop_data[['flight_path', 'map', 'drop_loc_cat']]
     scaler = MinMaxScaler()
-    drop_data.loc[:,:-1] = scaler.fit_transform(drop_data[drop_data.columns[:-1]])
+    drop_data.loc[:, :-1] = scaler.fit_transform(drop_data[drop_data.columns[:-1]])
     return drop_data
-
-
