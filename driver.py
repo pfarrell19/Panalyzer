d52ba536c941d65050ab65d7426876c442e65663
Samuel Sachnoff <ssachnof@calpoly.edu>
2019-06-06 23:25:21 -0700

    updated recomender to include stuff for drop location predictions

--- driver.py
+++ driver.py
@@ -1,17 +1,34 @@
 import pickle
 import sys
 
+from sklearn.externals import joblib
+
+import recommender
+
 def printHelp():
-    print('usage: python driver.py -l <map_name> [-h] [-p <inputfile>] [-d <inputfile>]\n')
+    print('usage: python driver.py -l <map_name> [-h] [-p <inputfile>] [-d]\n')
 
     print('Options')
     print('\t-h : help')
     print('\t-p : path prediction')
-    print('\t-d : drop location prediction')
-
+    print('\t-d : drop location prediction. Enter flight path as one of the cardinal directions.')
 
 def predict(model, input_data):
     return model.predict(input_data)
+
+
+def get_drop_model_file_path(map_name, flight_path):
+    '''
+    :param map_name: string
+    :param flight_path: string-> flight path of the plane
+    :returns: the path of the trained model given a map name and a flight path
+    '''
+
+
+    if map_name != 'Savage_Main':
+        return "./drop_models/model_" + map_name + "_" + flight_path + ".pkl"
+    return "./drop_models/model_Savage_Main.pkl"
+
 
 
 def main():
@@ -19,6 +36,8 @@ def main():
 
     path = False
     drop = False
+    flight_path = None
+    dropAll = True
 
     if '-h' in argv:
         printHelp()
@@ -36,16 +55,22 @@ def main():
         path = True
     
     if '-d' in argv:
-        dropInputFile = argv[argv.index('-d') + 1]
         drop = True
 
+
+
     if drop:
-        #TODO: Input data format; current assumption -> pickle df
-        dropInput = pickle.load(dropInputFile)
-        flight_direction = dropInput.iloc["flight_"]
-        modelDrop = pickle.load('''model''')
-        dropPrediction = predict(modelDrop, dropInput)
-        print ("Suggested Drop Location: {!r}".format(dropPrediction))
+        maps = ['Desert_Main', 'Erangel_Main', 'DihorOtok_Main', 'Savage_Main']
+        directions = ['nn', 'ne', 'ee', 'se', 'ss', 'sw', 'ww', 'nw']
+        for map in maps:
+            for direction in directions:
+                map_name = map
+                flight_path = direction
+                #TODO: Input data format; current assumption -> pickle df
+                model_file_path = get_drop_model_file_path(map_name, flight_path)
+                print(model_file_path)
+                model = joblib.load(model_file_path)
+                recommender.get_drop_predictions(map_name, flight_path, model)
 
 
     if path:
